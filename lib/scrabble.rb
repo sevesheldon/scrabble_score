34738a1cafc7d491ad9c9778b8231923ce4171ff
Seve Sheldon <sevesheldon@gmail.com>
2017-03-15 13:54:51 -0700

    add second working spec.

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,10 +1,10 @@
 class String
   define_method(:scrabble) do
     scrab_score = 0
-    one_point_letters = ["a"]
+    one_point_letters = ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"]
     string_split = self.split("")
     string_split.each() do |letter|
-      if letter = one_point_letters
+      if one_point_letters.include?(letter)
         scrab_score = scrab_score + 1
       end
     end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -6,4 +6,9 @@ describe('String#scrabble') do
   it('returns a scrabble score for a letter') do
     expect("a".scrabble()).to(eq(1))
   end
+
+  it('returns a scrabble score of 1 for all one-point letters') do
+    expect("e".scrabble()).to(eq(1))
+  end
+
 end
