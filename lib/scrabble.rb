20acccd2eb8f169aa9bc0e7008d8172e5561b2a0
Seve Sheldon <sevesheldon@gmail.com>
2017-03-15 14:42:54 -0700

    add the ninth and tenth working specs.

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,13 +1,15 @@
 class String
   define_method(:scrabble) do
     scrab_score = 0
-    one_point_letters = ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"]
-    two_point_letters = ["d", "g"]
-    three_point_letters = ["b", "c", "m", "p"]
-    four_point_letters = ["f", "h", "v", "w", "y"]
-    five_point_letter = "k"
-    eight_point_letters = ["j", "x"]
-    string_split = self.split("")
+    one_point_letters = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]
+    two_point_letters = ["D", "G"]
+    three_point_letters = ["B", "C", "M", "P"]
+    four_point_letters = ["F", "H", "V", "W", "Y"]
+    five_point_letter = "K"
+    eight_point_letters = ["J", "X"]
+    ten_point_letters = ["Q", "Z"]
+    string_to_upcase = self.upcase()
+    string_split = string_to_upcase.split("")
     string_split.each() do |letter|
       if one_point_letters.include?(letter)
         scrab_score = scrab_score + 1
@@ -20,7 +22,9 @@ class String
       elsif five_point_letter.include?(letter)
         scrab_score = scrab_score + 5
       elsif eight_point_letters.include?(letter)
-        scrab_score = scrab_score + 8  
+        scrab_score = scrab_score + 8
+      elsif ten_point_letters.include?(letter)
+        scrab_score = scrab_score + 10
       end
     end
     scrab_score
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -4,34 +4,42 @@ require('pry')
 
 describe('String#scrabble') do
   it('returns a scrabble score for a letter') do
-    expect("a".scrabble()).to(eq(1))
+    expect("A".scrabble()).to(eq(1))
   end
 
   it('returns a scrabble score of 1 for all one-point letters') do
-    expect("e".scrabble()).to(eq(1))
+    expect("E".scrabble()).to(eq(1))
   end
 
   it('returns a scrabble score equal to the sum of all the one-point letters included') do
-    expect("ae".scrabble()).to(eq(2))
+    expect("AE".scrabble()).to(eq(2))
   end
 
   it('returns a scrabble score equal to the sum of all the one-point and two-point letters included') do
-    expect("dog".scrabble()).to(eq(5))
+    expect("DOG".scrabble()).to(eq(5))
   end
 
   it('returns a scrabble score equal to the sum of all the one-point, two-point and three-point letters included') do
-    expect("blimp".scrabble()).to(eq(11))
+    expect("BLIMP".scrabble()).to(eq(11))
   end
 
   it('returns a scrabble score equal to the sum of all the one-point, two-point, three-point and four-point letters included') do
-    expect("help".scrabble()).to(eq(9))
+    expect("HELP".scrabble()).to(eq(9))
   end
 
   it('returns a scrabble score equal to the sum of all the one-point, two-point, three-point, four-point and five-point letters included') do
-    expect("kelp".scrabble()).to(eq(10))
+    expect("KELP".scrabble()).to(eq(10))
   end
 
   it('returns a scrabble score equal to the sum of all the one-point, two-point, three-point, four-point, five-point and eight-point letters included') do
-    expect("jack".scrabble()).to(eq(17))
+    expect("JACK".scrabble()).to(eq(17))
+  end
+
+  it('returns a scrabble score equal to the sum of all the letters included') do
+    expect("PIZZA".scrabble()).to(eq(25))
+  end
+
+  it('returns a scrabble score regardless of upper or lower casing') do
+    expect("PizZa".scrabble()).to(eq(25))
   end
 end
